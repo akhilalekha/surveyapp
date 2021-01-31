08c821e6c9cbe51ef845bc7d08afb7ea4dfa525c
akhilalekha <akhila160@gmail.com>
2021-02-01 00:21:51 +0530

    install redux-thunk, axios, create actions folder and first action skeleton

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,11 @@ import "materialize-css/dist/css/materialize.min.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
