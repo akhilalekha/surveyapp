33f5cf5a5a30073ed28bca88b872b6ed3beda80e
akhilalekha <akhila160@gmail.com>
2021-02-02 02:31:56 +0530

    add cases to the authreducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function authReducer(state = {}, action) {
-	console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+export default function authReducer(state = null, action) {
+	// console.log(action);
 	switch (action.type) {
+		case FETCH_USER:
+			return action.payload || false;
+
 		default:
 			return state;
 	}
